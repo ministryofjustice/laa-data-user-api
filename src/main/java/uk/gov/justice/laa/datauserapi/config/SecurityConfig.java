42808588663b458e02d874058d25678efee71343
carrb-moj <122389039+carrb-moj@users.noreply.github.com>
2026-08-13 15:46:26 +0100

    Update to allow access to prometheus endpoint (#47)

--- src/main/java/uk/gov/justice/laa/datauserapi/config/SecurityConfig.java
+++ src/main/java/uk/gov/justice/laa/datauserapi/config/SecurityConfig.java
@@ -2,11 +2,13 @@ package uk.gov.justice.laa.datauserapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.IpAddressMatcher;
 
 /**
  * Security configuration for laa-data-user-api.
@@ -26,6 +28,16 @@ public class SecurityConfig {
             )
             .authorizeHttpRequests(authorize -> authorize
                 .requestMatchers("/actuator/health", "/actuator/health/readiness", "/actuator/health/liveness", "/actuator/info").permitAll()
+                .requestMatchers("/actuator/prometheus")
+                .access((auth, context) -> {
+                    boolean allowed =
+                        new IpAddressMatcher("127.0.0.1").matches(context.getRequest())
+                            || new IpAddressMatcher("::1").matches(context.getRequest())
+                            || new IpAddressMatcher("10.0.0.0/8").matches(context.getRequest())
+                            || new IpAddressMatcher("172.16.0.0/12").matches(context.getRequest())
+                            || new IpAddressMatcher("192.168.0.0/16").matches(context.getRequest());
+                    return new AuthorizationDecision(allowed);
+                })
                 .anyRequest().authenticated()
             )
             .oauth2ResourceServer(oauth2 -> oauth2
